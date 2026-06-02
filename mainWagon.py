1af2d67e6e2d31bc7181666ba649e0a4f6435654
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-06-02 17:07:25 +0200

    bestfit
    implementation de la fonction bestfit1d oneline et offline.

--- mainWagon.py
+++ mainWagon.py
@@ -1,15 +1,20 @@
 from module.marchandises import Marchandises
 from module.train import Train
 from fonction.stockage1doffline import *
+from fonction.bestfit1d import *
 
 if __name__ == '__main__':
     marchandises = Marchandises()
-    train =Train()
+    train  = Train()
     train1 = Train()
+    train2 = Train()
+    train3 = Train()
     firstfit1doffline(marchandises, train)
     firstfit1doneline(marchandises, train1)
+    bestfit1doffline(marchandises, train2)
+    bestfit1doneline(marchandises, train3)
     # print("Le nombre de conteneur total est : ",len(cont));
-    print(train)
-    print(train1)
+    print(train3)
+    
 
 
