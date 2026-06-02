1b4598150ea344b030eaafdecd9a0985d46d53e7
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-06-02 09:04:27 +0200

    Modification du fichir Excel
    retrait d'un back slash n

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from fonction.notre_code import notrefonction
 
 if __name__ == "__main__":
     objet = Objets()
-    sac = notrefonction(objet.all, 0.6)
-
-    print(sac)
+    # sac = notrefonction(objet.all, 0.6)
+    sac1 = notrefonction(objet, 0.6)
+    print(sac1)
+   
 
