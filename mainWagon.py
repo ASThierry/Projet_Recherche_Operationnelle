036645ad0cd0e74cf2dce5218b6fdf67e45b0cb6
Mathis <mathis@exemple.com>
2026-06-02 14:23:05 +0200

    online 2d et divers affichage et calcule de temps

--- mainWagon.py
+++ mainWagon.py
@@ -1,5 +1,12 @@
 from module.marchandises import Marchandises
+from module.train import Train
+from fonction.Offline2d import guillotine2dOffline
+
+def testOffline2d():
+    marchandises = Marchandises()
+    train: Train = guillotine2dOffline(marchandises)
+    #print(train)
+
 
 if __name__ == '__main__':
-    marchandises = Marchandises()
-    print(marchandises)
+    testOffline2d()
