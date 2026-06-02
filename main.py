4bf6ed9941f046febebb79326ab2836d2b0af051
ASThierry <148657105+ASThierry@users.noreply.github.com>
2026-06-02 10:20:13 +0200

    Fonction heuristique
    amélioration de la fonction glouton nommé notre fonction,et modification du main.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from module.Objets import Objets,Objet
 from fonction.Brute_Force import brute_force
+from fonction.heuristique import heuristique
 from time import time
+
+def heurist():
+    objets = Objets()
+    print("FONCTION HEURISTIQUE : ")
+    sac = heuristique(objets.all,60)
+    print(sac)
+    print("FIN DE LA FONCTION HEURISTIQUE. ")
 
 if __name__ == "__main__":
 
@@ -8,7 +16,7 @@ if __name__ == "__main__":
     for i in sac.getall():
         print(i)
 
-    capacite_sac = 60
+    capacite_sac = 0.6
 
     utilite_max, meilleure_selection = brute_force(sac.all, capacite_sac)
 
@@ -18,6 +26,8 @@ if __name__ == "__main__":
     print("Objets à emporter :")
     for obj in meilleure_selection:
         print(f" - {obj.nom} ({obj.masse} g, utilité: {obj.utilite})")
+
+    heurist()
 
 
 
